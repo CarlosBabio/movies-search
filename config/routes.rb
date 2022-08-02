57fb996e8e2a653bd7ef8e25b1499eb031cf7f61
Carlos Babio <carlos.babio@live.com.au>
2022-08-02 17:04:36 +1000

    Created search controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'search#index'
+
+  get '/search', to: 'search#index'
+  get '/movie', to: 'search#movie'
 end
