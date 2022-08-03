f427c81ca34eb8e3ec0a5c41eb1dc6f877707dff
Carlos Babio <carlos.babio@live.com.au>
2022-08-03 19:31:20 +1000

    Fixed bug where not all movies were displaying bg colour properly and added some further styling to search box

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def index
+    @colours = %w[red green blue yellow]
     search = params[:q]
     if search
-      resp = Typhoeus.get("http://www.omdbapi.com/?apikey=#{ENV['OMDB_KEY']}&s", params: {s: search})
+      resp = Typhoeus.get("http://www.omdbapi.com/?apikey=#{ENV['OMDB_KEY']}&s", params: { s: search })
       @movies = JSON.parse(resp.body)["Search"]
     else
       @movies = []
@@ -12,22 +13,17 @@ class SearchController < ApplicationController
   def all
     @colours = %w[red green blue yellow]
     @all_colours_arr = []
+
+    # Searching for each colour individually then pushing each search result to an array
     @colours.each do |colour|
-      resp = Typhoeus.get("http://www.omdbapi.com/?apikey=#{ENV['OMDB_KEY']}&s", params: {s: colour})
+      resp = Typhoeus.get("http://www.omdbapi.com/?apikey=#{ENV['OMDB_KEY']}&s", params: { s: colour })
       @all_colours_arr << JSON.parse(resp.body)["Search"]
     end
   end
 
-  # Logic to extract colour from movie title
-  def bg_colour(movie_title)
-    title_arr = movie_title.downcase.split(' ')
-    match =  title_arr & @colours
-    match.first
-  end
-
   # Logic to get movie runtime from second API call using movie title given in first API call
   def movie_runtime(movie_title)
-    response = Typhoeus.get("http://www.omdbapi.com/?apikey=#{ENV['OMDB_KEY']}&s", params: {t: movie_title})
+    response = Typhoeus.get("http://www.omdbapi.com/?apikey=#{ENV['OMDB_KEY']}&s", params: { t: movie_title })
     JSON.parse(response.body)["Runtime"]
   end
 end
